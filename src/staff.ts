5f98801670c0e84b55cce71ce53b7f985fd9440a
Matthew Weidman <weidman.matthew@gmail.com>
2018-07-25 03:26:45 -0600

    treble clef is back

--- src/staff.ts
+++ src/staff.ts
@@ -13,15 +13,21 @@ class Staff {
     measureWidth: number;
     measures: Measure[];
 
-    constructor(pixelHeight: number, pixelWidth: number, 
-            innerHeight: number, innerWidth: number,
-            rowHeight: number, measureWidth: number) {
-        this.pixelHeight = pixelHeight;
-        this.pixelWidth = pixelWidth;
-        this.innerHeight = innerHeight;
-        this.innerWidth = innerWidth;
-        this.rowHeight = rowHeight;
-        this.measureWidth = measureWidth;
+    constructor() {
+
+        var frame = document.getElementById("musicframe");
+        var width = frame.clientWidth;
+        var height = frame.clientHeight;
+        console.log(frame);
+        
+        this.pixelHeight = height;
+        this.pixelWidth = width;
+
+        this.innerHeight = height;
+        this.innerWidth = width;
+
+        this.rowHeight = 200;
+        this.measureWidth = 100;
         this.measures = [];
     }
 
@@ -44,16 +50,30 @@ class Staff {
             new SVGAttr("viewBox", "0 0 " + this.innerWidth + " " + this.innerHeight)
         ]);
 
+        var rowg = new SVGElement("g", [
+            new SVGAttr("transform", "scale(" + this.rowHeight/100 + " " + this.rowHeight/100 + ")")
+        ]);
+        svg.addChild(rowg);
+
         // staff lines
         for (var i=0; i<5; i++) {
-            var y: number = (15 + i * 10) * this.rowHeight / 100;
-            svg.addChild(new SVGElement("line", [
+            var y: number = (15 + i * 10);
+            rowg.addChild(new SVGElement("line", [
                 new SVGAttr("x1", "0"),
                 new SVGAttr("y1", y + ""),
                 new SVGAttr("x2", this.measureWidth + ""),
                 new SVGAttr("y2", y + "")
             ]));
         }
+
+        // clef
+        rowg.addChild(new SVGElement("use", [
+            new SVGAttr("href", "./images/trebleclef.svg#svg1938"),
+            new SVGAttr("x", "0"),
+            new SVGAttr("y", "0"),
+            new SVGAttr("width", this.rowHeight + ""),
+            new SVGAttr("height", this.rowHeight + "")
+        ]));
 
         return svg;
     }
@@ -67,5 +87,5 @@ class Measure {
 }
 
 // create the staff
-var staff: Staff = new Staff(200, 200, 200, 200, 200, 200);
+var staff: Staff = new Staff();
 staff.display();
